ea2b230c6e232df4ff17aa7f51fb127e86b7a772
mitrathakker <mitra_thakker@yahoo.com>
2021-05-08 04:36:41 +0530

    Added codacy badge; minor refactoring in test_policy_validation.py

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ![](https://api.travis-ci.com/MitraThakker/AnyfinAssignment.svg?token=y3q591e6fq2n3iHnRknC&branch=master)
 [![Coverage Status](https://coveralls.io/repos/github/MitraThakker/CreditPolicyValidation/badge.svg?branch=master)](https://coveralls.io/github/MitraThakker/CreditPolicyValidation?branch=master)
+[![Codacy Badge](https://app.codacy.com/project/badge/Grade/7c4b81bc7ce24a5d801cb2b9c5224ce8)](https://www.codacy.com/gh/MitraThakker/CreditPolicyValidation/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=MitraThakker/CreditPolicyValidation&amp;utm_campaign=Badge_Grade)
--- tests/service/test_policy_validation.py
+++ tests/service/test_policy_validation.py
@@ -1,5 +1,7 @@
 import unittest
+
 import yaml
+
 from src.do.request import PolicyValidationRequest
 from src.do.response import ResponseResult
 from src.service.policy_validation import PolicyValidationService
@@ -35,91 +37,77 @@ class TestPolicyValidationService(unittest.TestCase):
         pass
 
     def test_run_accept(self):
-        request_ = PolicyValidationRequest.from_dict({
-            'customer_income': 1000,
-            'customer_debt': 500,
-            'payment_remarks_12m': 0,
-            'payment_remarks': 1,
-            'customer_age': 18
-        })
+        request_ = PolicyValidationRequest(customer_income=1000,
+                                           customer_debt=500,
+                                           payment_remarks_12m=0,
+                                           payment_remarks=1,
+                                           customer_age=18)
         response_ = PolicyValidationService(request_, self.base_config).run().as_dict()
 
         self.assertEqual(ResponseResult.ACCEPT.value, response_.get('result'))
         self.assertEqual('', response_.get('reason'))
 
     def test_low_income(self):
-        request_ = PolicyValidationRequest.from_dict({
-            'customer_income': 499,
-            'customer_debt': 500,
-            'payment_remarks_12m': 0,
-            'payment_remarks': 1,
-            'customer_age': 18
-        })
+        request_ = PolicyValidationRequest(customer_income=499,
+                                           customer_debt=500,
+                                           payment_remarks_12m=0,
+                                           payment_remarks=1,
+                                           customer_age=18)
         response_ = PolicyValidationService(request_, self.base_config).run().as_dict()
 
         self.assertEqual(ResponseResult.REJECT.value, response_.get('result'))
         self.assertEqual('LOW_INCOME', response_.get('reason'))
 
     def test_high_debt(self):
-        request_ = PolicyValidationRequest.from_dict({
-            'customer_income': 1000,
-            'customer_debt': 501,
-            'payment_remarks_12m': 0,
-            'payment_remarks': 1,
-            'customer_age': 18
-        })
+        request_ = PolicyValidationRequest(customer_income=1000,
+                                           customer_debt=501,
+                                           payment_remarks_12m=0,
+                                           payment_remarks=1,
+                                           customer_age=18)
         response_ = PolicyValidationService(request_, self.base_config).run().as_dict()
 
         self.assertEqual(ResponseResult.REJECT.value, response_.get('result'))
         self.assertEqual('HIGH_DEBT_FOR_INCOME', response_.get('reason'))
 
     def test_payment_remarks_12m(self):
-        request_ = PolicyValidationRequest.from_dict({
-            'customer_income': 1000,
-            'customer_debt': 500,
-            'payment_remarks_12m': 1,
-            'payment_remarks': 1,
-            'customer_age': 18
-        })
+        request_ = PolicyValidationRequest(customer_income=1000,
+                                           customer_debt=500,
+                                           payment_remarks_12m=1,
+                                           payment_remarks=1,
+                                           customer_age=18)
         response_ = PolicyValidationService(request_, self.base_config).run().as_dict()
 
         self.assertEqual(ResponseResult.REJECT.value, response_.get('result'))
         self.assertEqual('PAYMENT_REMARKS_12M', response_.get('reason'))
 
     def test_payment_remarks(self):
-        request_ = PolicyValidationRequest.from_dict({
-            'customer_income': 1000,
-            'customer_debt': 500,
-            'payment_remarks_12m': 0,
-            'payment_remarks': 2,
-            'customer_age': 18
-        })
+        request_ = PolicyValidationRequest(customer_income=1000,
+                                           customer_debt=500,
+                                           payment_remarks_12m=0,
+                                           payment_remarks=2,
+                                           customer_age=18)
         response_ = PolicyValidationService(request_, self.base_config).run().as_dict()
 
         self.assertEqual(ResponseResult.REJECT.value, response_.get('result'))
         self.assertEqual('PAYMENT_REMARKS', response_.get('reason'))
 
     def test_underage(self):
-        request_ = PolicyValidationRequest.from_dict({
-            'customer_income': 1000,
-            'customer_debt': 500,
-            'payment_remarks_12m': 0,
-            'payment_remarks': 1,
-            'customer_age': 17
-        })
+        request_ = PolicyValidationRequest(customer_income=1000,
+                                           customer_debt=500,
+                                           payment_remarks_12m=0,
+                                           payment_remarks=1,
+                                           customer_age=17)
         response_ = PolicyValidationService(request_, self.base_config).run().as_dict()
 
         self.assertEqual(ResponseResult.REJECT.value, response_.get('result'))
         self.assertEqual('UNDERAGE', response_.get('reason'))
 
     def test_invalid_config(self):
-        request_ = PolicyValidationRequest.from_dict({
-            'customer_income': 1000,
-            'customer_debt': 500,
-            'payment_remarks_12m': 0,
-            'payment_remarks': 1,
-            'customer_age': 18
-        })
+        request_ = PolicyValidationRequest(customer_income=1000,
+                                           customer_debt=500,
+                                           payment_remarks_12m=0,
+                                           payment_remarks=1,
+                                           customer_age=18)
         with self.assertRaises(yaml.YAMLError):
             PolicyValidationService(request_, {}).run().as_dict()
 
